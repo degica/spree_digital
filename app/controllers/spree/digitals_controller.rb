505834ba512135e2895dbd87371a79adc1c390ae
camelmasa <camelmasa@gmail.com>
2014-08-08 10:33:27 +0900

    Update digitals_controller.rb from master branch
    It taken from:
    
    https://github.com/halo/spree_digital/blob/master/app/controllers/spree/digitals_controller.rb
    https://github.com/halo/spree_digital/blob/master/spec/controllers/digitals_controller_spec.rb
    https://github.com/halo/spree_digital/blob/master/spec/factories/digital_factory.rb

--- app/controllers/spree/digitals_controller.rb
+++ app/controllers/spree/digitals_controller.rb
@@ -1,19 +1,19 @@
 module Spree
   class DigitalsController < Spree::StoreController
     ssl_required :show
+    rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
 
     def show
-      link = DigitalLink.find_by_secret(params[:secret])
-
-      if link.present? and link.digital.attachment.present?
-        attachment = link.digital.attachment
-
+      if attachment.present?
         # don't authorize the link unless the file exists
         # the logger error will help track down customer issues easier
-
-        if File.file?(attachment.path)
-          if link.authorize!
-            send_file attachment.path, :filename => attachment.original_filename, :type => attachment.content_type and return
+        if attachment_is_file?
+          if digital_link.authorize!
+            if Paperclip::Attachment.default_options[:storage] == :s3
+              redirect_to attachment.expiring_url(Spree::DigitalConfiguration[:s3_expiration_seconds]) and return
+            else
+              send_file attachment.path, :filename => attachment.original_filename, :type => attachment.content_type and return
+            end
           end
         else
           Rails.logger.error "Missing Digital Item: #{attachment.path}"
@@ -22,6 +22,28 @@ module Spree
 
       render :unauthorized
     end
+
+    private
+
+      def attachment_is_file?
+        if Paperclip::Attachment.default_options[:storage] == :s3
+          attachment.exists?
+        else
+          File.file?(attachment.path)
+        end
+      end
+
+      def digital_link
+        @link ||= DigitalLink.find_by!(secret: params[:secret])
+      end
+
+      def attachment
+        @attachment ||= digital_link.digital.try(:attachment) if digital_link.present?
+      end
+
+      def resource_not_found
+        head status: 404
+      end
 
   end
 end

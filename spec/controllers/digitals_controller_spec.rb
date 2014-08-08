293a3d6f84c42e237af717c3366e4c98ce14d699
camelmasa <camelmasa@gmail.com>
2014-08-08 11:40:06 +0900

    Create other resource url logic
    Closes #1. [#76033794]

--- spec/controllers/digitals_controller_spec.rb
+++ spec/controllers/digitals_controller_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Spree::DigitalsController do
 
   context '#show' do
-    let(:digital) { create(:digital) }
     let(:authorized_digital_link) { create(:digital_link, digital: digital) }
 
     it 'returns a 404 for a non-existent secret' do
@@ -11,33 +10,45 @@ describe Spree::DigitalsController do
       response.code.should eq('404')
     end
 
-    it 'returns a 200 and calls send_file for link that is not a file' do
-      controller.should_receive(:attachment_is_file?).and_return(false)
-      controller.should_not_receive(:send_file)
-      spree_get :show, secret: authorized_digital_link.secret
-      response.code.should eq('200')
-      response.should render_template(:unauthorized)
+    context 'when exists attachment file' do
+      let(:digital) { create(:digital) }
+      it 'returns a 200 and calls send_file for link that is not a file' do
+        controller.should_receive(:attachment_is_file?).and_return(false)
+        controller.should_not_receive(:send_file)
+        spree_get :show, secret: authorized_digital_link.secret
+        response.code.should eq('200')
+        response.should render_template(:unauthorized)
+      end
+
+      it 'returns a 200 and calls send_file for an authorized link that is a file' do
+        controller.should_receive(:attachment_is_file?).and_return(true)
+        controller.should_receive(:send_file).with(digital.attachment.path,
+                                                   filename: digital.attachment.original_filename,
+                                                   type: digital.attachment.content_type).
+                                              and_return{controller.render nothing: true,
+                                                                           content_type: digital.attachment.content_type }
+        spree_get :show, secret: authorized_digital_link.secret
+        response.code.should eq('200')
+        response.header['Content-Type'].should match digital.attachment.content_type
+      end
+
+      it 'redirects to s3 for an authorized link when using s3' do
+        skip 'TODO: needs a way to test without having a bucket'
+        Paperclip::Attachment.default_options[:storage] = :s3
+        controller.should_receive(:redirect_to)
+        controller.should_receive(:attachment_is_file?).and_return(true)
+        controller.should_not_receive(:send_file)
+        spree_get :show, secret: authorized_digital_link.secret
+      end
     end
 
-    it 'returns a 200 and calls send_file for an authorized link that is a file' do
-      controller.should_receive(:attachment_is_file?).and_return(true)
-      controller.should_receive(:send_file).with(digital.attachment.path,
-                                                 :filename => digital.attachment.original_filename,
-                                                 :type => digital.attachment.content_type).
-                                            and_return{controller.render :nothing => true,
-                                                                         :content_type => digital.attachment.content_type }
-      spree_get :show, secret: authorized_digital_link.secret
-      response.code.should eq('200')
-      response.header['Content-Type'].should match digital.attachment.content_type
-    end
-
-    it 'redirects to s3 for an authorized link when using s3' do
-      skip 'TODO: needs a way to test without having a bucket'
-      Paperclip::Attachment.default_options[:storage] = :s3
-      controller.should_receive(:redirect_to)
-      controller.should_receive(:attachment_is_file?).and_return(true)
-      controller.should_not_receive(:send_file)
-      spree_get :show, secret: authorized_digital_link.secret
+    context 'when not exist attachment file' do
+      let(:digital) { create(:digital, attachment_content_type: nil,
+                                       attachment_file_name: nil) }
+      before { spree_get :show, secret: authorized_digital_link.secret }
+      subject { response }
+      its(:code) { should == '302' }
+      its(:redirect_url) { should == digital.resource_url }
     end
   end
 end
